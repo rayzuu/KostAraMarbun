7a6ed7aa1f4025a186ac1b5cebe1b96e27d42527
rayzuu <zidaneraihan24@gmail.com>
2026-05-11 21:54:13 +0700

    update css landing, public

--- resources/views/layouts/landingPage.blade.php
+++ resources/views/layouts/landingPage.blade.php
@@ -4,7 +4,8 @@
 
     @section('content')
 
-   <section class="hero-section">
+   <section class="hero-section" style="background-image: url('{{ asset('image/kost.jpg') }}');">>
+    
 
     <div class="hero-overlay"></div>
 

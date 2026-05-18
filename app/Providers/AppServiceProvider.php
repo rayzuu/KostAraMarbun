83695148368d78e5c4375bc69ff6f5cc7519a4d0
rayzuu <zidaneraihan24@gmail.com>
2026-05-18 22:31:26 +0700

    app service provider update carbon manip waktu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
    public function boot(): void
 {
-    
+    /**
+    Carbon::setTestNow('2026-06-01 10:00:00');
+    */
     URL::forceScheme('https');
     Paginator::useBootstrapFive();
 }
